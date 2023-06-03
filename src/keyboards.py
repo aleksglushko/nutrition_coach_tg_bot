3155ea72e22799cfab2a5e0ae09dbc9f0d5131f5
spaceiling <aleksandr.glushko@ivx.com>
2023-06-03 20:42:07 +0200

    fix msgs

--- src/keyboards.py
+++ src/keyboards.py
@@ -2,7 +2,7 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardBut
 
 greet_kb = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True
-).add(KeyboardButton('Привет, Коуч! 👋'))
+).add(KeyboardButton('Привет! 👋'))
 
 gender_keyboard = ReplyKeyboardMarkup(
     resize_keyboard=True, one_time_keyboard=True
